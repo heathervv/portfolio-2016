c313da124d1f68ba7b1bdf03626c05e1abb73116
Heather Vandervecht <heather@res.im>
2015-10-29 13:53:50 -0400

    Deleting site 🙊

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-# heathervv.github.io
-Personal Portfolio
--- _site/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var cta = $('.cta');
-
-	cta.click(function() {
-		$("html, body").animate({ scrollTop: $(document).height() }, 750, 'swing');
-
-		return false;
-	});
-});
