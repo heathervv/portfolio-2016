2191373380eb88d85b16ce6e8a90d5c7d5f05109
Heather Vandervecht <heather@res.im>
2015-11-07 23:00:18 -0500

    Playful gradient

--- js/script.js
+++ js/script.js
@@ -21,4 +21,32 @@ function init(){
 		  window.scrollBy(0,scrollAmount);
 		}
 	}
+
+	// var reveal = document.getElementsByClassName('intro__content')[0].getElementsByTagName("a");
+
+	// for(var i = 0, len = reveal.length; i < len; i++) {
+	// 	reveal[i].addEventListener('mouseover',function() {
+	// 		document.getElementsByTagName('body')[0].className = 'hovered';
+	// 	},false);
+	// 	reveal[i].addEventListener('mouseout',function() {
+	// 		document.getElementsByTagName('body')[0].className -= 'hovered';
+	// 	},false);
+	// }
 };
+
+function getMousePos(evt) {
+    var doc = document.documentElement || document.body;
+    var pos = {
+        x: evt ? evt.pageX : window.event.clientX + doc.scrollLeft - doc.clientLeft,
+        y: evt ? evt.pageY : window.event.clientY + doc.scrollTop - doc.clientTop
+    };
+    return pos;
+}
+
+document.onmousemove = moveMouse;
+
+function moveMouse(evt) {
+    var pos = getMousePos(evt),
+		gradient = document.getElementById("_gradient");
+    gradient.style.backgroundPosition = pos.x + "px " + pos.y + "px";
+}
